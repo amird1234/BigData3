62f1832159f9014673b279ee88f7c832eab8e042
AvnerGidron305 <AvnerGidron1@gmail.com>
2016-07-02 18:45:03 +0300

    added names in begining of all files

--- final-project/src/main/java/enums/CommandType.java
+++ final-project/src/main/java/enums/CommandType.java
@@ -1,5 +1,11 @@
 package enums;
 
+/*
+Avner Gidron; AvnerGidron1@gmail.com; 201533262
+Carmi Arlinsky; 4carmi@gmail.com; 029993904
+Samah Ghazawi; idrees.samah@gmail.com; 301416897
+Amir dahan; Amird1234@gmail.com; 039593801
+*/
 public enum CommandType {
 	PAGE_RANK("pagerank",1),
 	MAP_COMMAND("map",2),
--- final-project/src/main/java/univ/bigdata/course/movie/PageRankResults.java
+++ final-project/src/main/java/univ/bigdata/course/movie/PageRankResults.java
@@ -1,3 +1,9 @@
+/*
+Avner Gidron; AvnerGidron1@gmail.com; 201533262
+Carmi Arlinsky; 4carmi@gmail.com; 029993904
+Samah Ghazawi; idrees.samah@gmail.com; 301416897
+Amir dahan; Amird1234@gmail.com; 039593801
+*/
 package univ.bigdata.course.movie;
 
 
